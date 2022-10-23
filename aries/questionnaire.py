52ad5b3885fe6a30298f9622a31d97c48ca30769
01nietzsch <vladimirjurien@gmail.com>
2022-10-23 15:17:11 +0100

    vhanged questions

--- aries/questionnaire.py
+++ aries/questionnaire.py
@@ -1,19 +1,23 @@
 import json
 
-print("Vous allez desormais reepondre a quelques questions ")
+print("You will now answer some questions about the treatment.  ")
 
-question = ['avez vous de la toux',
-             "avez vous des pbs gastriques",
-             "maux de tetes vertiges frissons ",
-             "dificultées respiratoires",
-             "depression",
-             "perte de gouts et d'odorat"
+question = ['Do you have a cough ? ',
+             "Do you have any gastric problems ? ",
+             "Do you have headaches ? ",
+             "Do you have dizziness  ? ",
+             "Do you have chills ? ",
+             "Do you have breathing difficulties ? ",
+             "Do you feel depressed ? ",
+             "Do you feel a loss of taste ? ",
+             "Do you feel a loss of smell ? "
              ]
 reponse = []
 
 for questions in question:
     reponses = input(f"{questions} ?")
     reponse.append(reponses)
+    print('Thank you very much for your participation, we are very grateful. We will contact you in the next few days. See you soon!')
 
 with open('answers.json', 'w', encoding='utf-8') as f:
     json.dump(reponse, f, ensure_ascii=False, indent=4)
